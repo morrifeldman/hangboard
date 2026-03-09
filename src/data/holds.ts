2d8bf358022a0d096af19c6c83743960064cdff4
morri <morrifeldman@gmail.com>
2026-03-08 20:31:11 -0400

    Fix IMR Shallow default base to -10

--- src/data/holds.ts
+++ src/data/holds.ts
@@ -25,7 +25,7 @@ export const HOLDS: HoldDefinition[] = [
   { id: "large-edge",  name: "Large Edge",  defaultSet1Weight:  5,    defaultSet2Weight:  15,   set1Reps: 7, set2Reps: 6 },
   { id: "mr-shallow",  name: "MR Shallow",  defaultSet1Weight: -35,   defaultSet2Weight: -25,   set1Reps: 7, set2Reps: 6 },
   { id: "small-edge",  name: "Med Edge",    defaultSet1Weight: -20,   defaultSet2Weight: -10,   set1Reps: 7, set2Reps: 6 },
-  { id: "imr-shallow", name: "IMR Shallow", defaultSet1Weight:  10,   defaultSet2Weight:  0,    set1Reps: 7, set2Reps: 6 },
+  { id: "imr-shallow", name: "IMR Shallow", defaultSet1Weight:  -10,   defaultSet2Weight:  0,    set1Reps: 7, set2Reps: 6 },
   { id: "wide-pinch",  name: "Wide Pinch",  defaultSet1Weight: -45,   defaultSet2Weight: -35,   set1Reps: 7, set2Reps: 6 },
   { id: "sloper",      name: "Sloper",      defaultSet1Weight: -17.5, defaultSet2Weight: -7.5,  set1Reps: 7, set2Reps: 6 },
   { id: "med-pinch",   name: "Med Pinch",   defaultSet1Weight: -50,   defaultSet2Weight: -40,   set1Reps: 7, set2Reps: 6 },
